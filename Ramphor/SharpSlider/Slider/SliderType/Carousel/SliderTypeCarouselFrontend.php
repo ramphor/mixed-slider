06a9784abc71b3200748a6583b0060ad6c27a6a9
Puleeno Nguyen <puleeno@gmail.com>
2023-02-15 09:42:38 +0700

    migrate to use swiffy-slider

--- Ramphor/SharpSlider/Slider/SliderType/Carousel/SliderTypeCarouselFrontend.php
+++ Ramphor/SharpSlider/Slider/SliderType/Carousel/SliderTypeCarouselFrontend.php
@@ -15,22 +15,17 @@ class SliderTypeCarouselFrontend extends AbstractSliderTypeFrontend
         $sliderId = empty($this->slider->alias) ? "sharp_slider_{$this->slider->sliderId}" : $this->slider->alias;
         $slides = $this->slider->getSlides();
         ?>
-        <div id="<?php echo $sliderId; ?>" class="splide">
-            <div class="splide__track">
-                <ul class="splide__list">
-                <?php foreach ($slides as $slide) : ?>
-                    <li class="splide__slide"><?php echo $slide->background; ?></li>
-                <?php endforeach; ?>
-                </ul>
-            </div>
+        <div id="<?php echo $sliderId; ?>" class="swiffy-slider slider-item-show4 slider-nav-page slider-nav-autoplay slider-nav-autopause" data-slider-nav-autoplay-interval="5000">
+
+            <ul class="slider-container">
+            <?php foreach ($slides as $slide) : ?>
+                <li class="splide__slide"><?php echo $slide->background; ?></li>
+            <?php endforeach; ?>
+            </ul>
+            <button type="button" class="slider-nav" aria-label="Go left"></button>
+            <button type="button" class="slider-nav slider-nav-next" aria-label="Go left"></button>
         </div>
 
         <?php
-        execute_script("<script>var {$sliderId} = new Splide( '#{$sliderId}', {
-            type: 'loop',
-            perPage: 4,
-            perMove: 4,
-            pagination: false,
-        } ).mount();</script>");
     }
 }
